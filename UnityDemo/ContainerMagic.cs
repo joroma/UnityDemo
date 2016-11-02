7e7af464bf84514440453d34bc81e0d99f8806bd
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-02 11:48:00 +0800

    Finish tutorial

--- UnityDemo/ContainerMagic.cs
+++ UnityDemo/ContainerMagic.cs
@@ -6,15 +6,26 @@ namespace UnityDemo
     {
         public static void RegisterElements(IUnityContainer container)
         {
-            Dial dial = new Dial("Linear");
-            container.RegisterInstance(dial);
+            container.RegisterTypes(
+                AllClasses.FromLoadedAssemblies(),
+                WithMappings.FromMatchingInterface,
+                WithName.Default,
+                WithLifetime.ContainerControlled);
 
-            container.RegisterType<IBattery, Battery>();
-            container.RegisterType<ITuner, Tuner>();
+            container.RegisterType<Casing>(new InjectionConstructor("Plastic"));
 
-            var batteryType = typeof(IBattery);
-            var tunerType = typeof(ITuner);
-            container.RegisterType<IRadio, Radio>(new InjectionConstructor(batteryType, tunerType, typeof(string)));
+            //Dial dial = new Dial("Linear");
+            //container.RegisterInstance(dial);
+
+            //container.RegisterType<IBattery, Battery>();
+            //container.RegisterType<ITuner, Tuner>();
+
+            //container.RegisterType<ISpeaker, CheapSpeaker>("Cheap");
+            //container.RegisterType<ISpeaker, PriceySpeaker>("Expensive");
+
+            //var batteryType = typeof(IBattery);
+            //var tunerType = typeof(ITuner);
+            //container.RegisterType<IRadio, Radio>(new InjectionConstructor(batteryType, tunerType, typeof(string)));
         }
     }
 }
--- UnityDemo/Program.cs
+++ UnityDemo/Program.cs
@@ -17,14 +17,19 @@ namespace UnityDemo
             IBattery battery = container.Resolve<IBattery>();
             Console.WriteLine(battery.SerialNumber());
 
-            Dial dial = container.Resolve<Dial>();
-            Console.WriteLine(dial.DialType());
+            
 
             ITuner tuner = container.Resolve<ITuner>();
-            IRadio radio = container.Resolve<IRadio>(new ParameterOverride("radioBattery", battery),
-                new ParameterOverride("radioTuner", tuner),
-                new ParameterOverride("radioName", "BrokenRadio"));
-            radio.Start();
+            Console.WriteLine(tuner.SerialNumber());
+            //IRadio radio = container.Resolve<IRadio>(new ParameterOverride("radioBattery", battery),
+            //    new ParameterOverride("radioTuner", tuner),
+            //    new ParameterOverride("radioName", "BrokenRadio"));
+            //radio.Start();
+
+            //ISpeaker cheapSpeaker = container.Resolve<ISpeaker>("Cheap");
+            //ISpeaker priceySpeaker = container.Resolve<ISpeaker>("Expensive");
+            //cheapSpeaker.Start();
+            //priceySpeaker.Start();
 
             Console.ReadLine();
         }
